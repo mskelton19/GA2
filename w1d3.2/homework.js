d60e4190acb72fd415ceb00a9a8319c9f013d918
Mike Skelton <skeltonmr43@gmail.com>
2018-08-26 12:55:30 -0700

    w1d3 homework

---
+++ w1d3.2/homework.js
@@ -0,0 +1,89 @@
+// for(i = 1; i <= 20; i++){
+//   console.log(i);
+// }
+
+// for(i = 2; i <= 200; i+=2){
+//   console.log(i);
+// }
+
+// function getRandomInt(max){
+//   myNumber = Math.floor(Math.random() * Math.floor(max));
+//   // console.log(myNumber);
+// }
+//
+// for(i=1; i <=20; i++){
+//   if(i % 2 != 0){
+//     console.log("Love me, pet me! HSSSSS");
+//   } else {
+//     getRandomInt(3);
+//     // console.log(myNumber)
+//     if(myNumber === 0){
+//       console.log("human..why are you taking pictures")
+//     } else if(myNumber === 1){
+//       console.log("the catnip made me do it")
+//     } else {
+//       console.log("why does the red dot get away");
+//     }
+//     }
+//   }
+
+
+// for(i = 1; i <= 100; i++){
+//   if(i % 3 === 0 && i % 5 === 0){
+//     console.log("Fizzbuzz")
+//   } else if(i % 5 === 0){
+//     console.log("Buzz")
+//   } else if(i % 3 === 0){
+//     console.log("Fizz")
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+
+// const thom = ["Thom", 1000, "Christchurch"];
+// const karolin = ["Karolin", 16, "New York"];
+// const kristyn = ["Kristyn", 5, "Pittsburgh"];
+// const matt = ["Matt", 186, "Philadelphia"];
+//
+// thom[0] = "Gameboy";
+// karolin[1] = 17;
+// matt[2] = "Gotham City";
+// kristyn[2] = "Brooklyn";
+//
+// console.log(thom);
+// console.log(karolin);
+// console.log(kristyn);
+// console.log(matt);
+
+// var sum = 0;
+//
+// for(i = 1; i < 1000; i++){
+//   if(i % 3 === 0){
+//     sum += i;
+//   } else if(i % 5 === 0){
+//     sum += i;
+//   }
+// }
+//
+// console.log(sum);
+
+// var myTriangle = "";
+// var height = 7;
+
+// for(i = 1; i <= 7; i++){
+//   myTriangle += "+"
+//   console.log(myTriangle);
+// }
+
+// for(i = height; i > 0; i--){
+//   console.log("#".repeat(i));
+// }
+
+// const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+//
+// for(i = 0; i < nums.length; i++){
+//   nums.sort(function(a, b){return a - b});
+// }
+//
+// console.log(nums[(nums.length/2)+.5])
