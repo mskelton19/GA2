73deb301404c619b63d536eba61771550393feba
Mike Skelton <skeltonmr43@gmail.com>
2018-09-27 11:36:39 -0400

    objects

---
+++ w2d3/objects.js
@@ -0,0 +1,56 @@
+// const car = {color : "red", hp: 500, year: 2013 };
+//
+// console.log(car);
+// console.log(car.color);
+
+// const person = {name: "Mike", age: 29, school: "Carnegie Mellon"}
+//
+// console.log(person.school);
+
+// const dog = {name: "Beefy", breed: "Pitty", age: 4}
+//
+// console.log(dog.name);
+
+// const celebrity = {name: "Elijah Wood", age: 40, isCurrentlyTweeting: true}
+//
+// if(celebrity.isCurrentlyTweeting === true){
+//   console.log("Turn off Twitter");
+// }
+
+// building a house
+// const house = {doors: 9}
+//
+// house.windows = 30;
+// house.garden = true;
+// house.garden = false;
+//
+// console.log(house);
+
+
+// macros
+// const macros = {}
+//
+// macros.protein = "tempeh";
+// macros.carbohydrates = "spuds";
+// macros.fats= "olive oil";
+//
+// console.log(macros);
+//
+// // guitar
+// const guitar = {}
+//
+// guitar.strings = 6;
+// guitar.isAcoustic = true;
+// guitar.strings = 100;
+// guitar.isAcoustic = false;
+// guitar.belongsTo = "Dimebag Darrell"
+//
+// console.log(guitar.belongsTo);
+
+// const testObject = {}
+//
+// testObject['this is a test'] = "test";
+// testObject['2'] = "I'm just messing around with objects";
+//
+// console.log(testObject['this is a test']);
+// console.log(testObject['2']);
