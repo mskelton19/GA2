36ba756dbab912b2130525bf2e63c0fa1a2374c0
Mike Skelton <skeltonmr43@gmail.com>
2018-09-26 21:57:01 -0400

    buy/use lawnmower

--- w1d5/landscaper/landscaper.js
+++ w1d5/landscaper/landscaper.js
@@ -1,24 +1,13 @@
 let money = 0;
 let days = 0;
 let scissors = 0;
+let pushMower = 0;
 
 let cutTeeth = () => {
   money += 1;
   days += 1;
   console.log("Days: " + days + " Money: " + money);
   game();
-}
-
-let cutScissors = () => {
-  if(scissors == 1){
-  money += 5;
-  days += 1;
-  console.log("Days: " + days + " Money: " + money);
-  game();
-} else {
-  alert("You don't own any scissors");
-  game();
-}
 }
 
 let buyScissors = () => {
@@ -36,6 +25,46 @@ let buyScissors = () => {
   }
 }
 
+let cutScissors = () => {
+  if(scissors == 1){
+  money += 5;
+  days += 1;
+  console.log("Days: " + days + " Money: " + money);
+  game();
+} else {
+  alert("You don't own any scissors");
+  game();
+}
+}
+
+let buyPush = () => {
+  if(money >= 25 && scissors == 1 && pushMower < 1){
+    pushMower += 1;
+    money -= 25;
+    console.log("Days: " + days + " Money: " + money);
+    game();
+  } else if(money < 25){
+    alert("You don't have enough for a push mower");
+    game();
+  } else if(scissors < 1) {
+    alert("You must buy scissors first");
+  } else {
+    alert("You already own a push mower");
+    game();
+  }
+}
+
+let cutPushMower = () => {
+  if(pushMower == 1){
+  days += 1;
+  money += 50;
+  console.log("Days: " + days + " Money: " + money);
+  game();
+} else {
+  alert("You don't have a push mower");
+}
+}
+
 const game = () => {
 // Check what action the user wants to take. Buy/work
 let toDo = prompt("What do you want to do?", "Work, Buy");
@@ -50,6 +79,8 @@ if(toDo == "work" || toDo === "Work"){
     cutTeeth();
   } else if(equipment === "Scissors" || equipment === "scissors"){
     cutScissors();
+  } else if(equipment === "Push Mower" || equipment === "push mower" || equipment === "Push mower"){
+    cutPushMower();
   }
 
 // If they say Buy
@@ -61,6 +92,8 @@ if(toDo == "work" || toDo === "Work"){
   // if they choose to buy Scissors
   if(purchase === "Scissors" || purchase === "scissors"){
     buyScissors();
+  } else if(purchase === "Push Mower" || purchase === "push mower" || purchase === "Push mower"){
+    buyPush();
   }
 }
 }
