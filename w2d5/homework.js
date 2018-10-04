2be06dd1437b176fc1c52bab639ed6e2e53564df
Mike Skelton <skeltonmr43@gmail.com>
2018-10-04 11:48:14 -0400

    methods in an object

--- w2d5/homework.js
+++ w2d5/homework.js
@@ -1,29 +1,46 @@
-let clicker = {
-  clickCount: 0,
-  click(){
-    this.clickCount += 1;
-    console.log(this.clickCount);
+// let clicker = {
+//   clickCount: 0,
+//   click(){
+//     this.clickCount += 1;
+//     console.log(this.clickCount);
+//   }
+// }
+// // clicker.click();
+//
+// const myInformation = {
+//   name: "Mike",
+//   age: 29,
+//   fact: "I am pursuing my MBA",
+//   statement(){
+//     return ("My name is " + this.name + ", I'm " + this.age + " and I'm " + this.fact)
+//   },
+//   addAge(){
+//     return(this.age += 1);
+//   },
+//   changeName(){
+//     return(this.name = "Michael");
+//   }
+// }
+//
+// // console.log(myInformation.statement());
+// // console.log(myInformation.addAge());
+//
+// console.log(myInformation.age + 5);
+// console.log(myInformation.changeName());
+
+//  Greeter
+const greeter = {
+  hello(name){
+    return("Hello, " + name + ". How are you today?")
+  },
+  goodbye(name){
+    return("Peace out, " + name + "!")
+  },
+  whoAreYou(name){
+    return("Oh right! " + name + "! How could I forget?")
   }
 }
-// clicker.click();
 
-const myInformation = {
-  name: "Mike",
-  age: 29,
-  fact: "I am pursuing my MBA",
-  statement(){
-    return ("My name is " + this.name + ", I'm " + this.age + " and I'm " + this.fact)
-  },
-  addAge(){
-    return(this.age += 1);
-  },
-  changeName(){
-    return(this.name = "Michael");
-  }
-}
-
-// console.log(myInformation.statement());
-// console.log(myInformation.addAge());
-
-console.log(myInformation.age + 5);
-console.log(myInformation.changeName());
+console.log(greeter.hello("Mike"));
+console.log(greeter.goodbye("Billy"));
+console.log(greeter.whoAreYou("Emma"));
